98fbfa25eff963c3257d4b08d30c6d4e1b1c1f6a
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-02-12 17:47:47 +0600

    favicon added

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let SubhanAllahValue = 0;
 let AlhamdulillahValue = 0;
 let AllahuAkberValue = 0;
 
-document.getElementById('subhan-allah-increment-btn').addEventListener('click',function(){
-    if(SubhanAllahValue>=33){
+document.getElementById('subhan-allah-increment-btn').addEventListener('click', function () {
+    if (SubhanAllahValue >= 33) {
         return alert('Now Start Alhamdulillah')
     }
     SubhanAllahValue++;
     let counter = document.getElementById('subhan-allah-display');
     counter.innerText = SubhanAllahValue;
 })
-document.getElementById('subhan-allah-decrement-btn').addEventListener('click',function(){
-    if(SubhanAllahValue<=0){
+document.getElementById('subhan-allah-decrement-btn').addEventListener('click', function () {
+    if (SubhanAllahValue <= 0) {
         return alert('Only positive value Available!')
     }
     SubhanAllahValue--;
@@ -20,16 +20,16 @@ document.getElementById('subhan-allah-decrement-btn').addEventListener('click',f
 })
 
 
-document.getElementById('alhamdulillah-increment-btn').addEventListener('click',function(){
-    if(AlhamdulillahValue>=33){
+document.getElementById('alhamdulillah-increment-btn').addEventListener('click', function () {
+    if (AlhamdulillahValue >= 33) {
         return alert('Now Start Allahu Akbar')
     }
     AlhamdulillahValue++;
     let counter = document.getElementById('alhamdulillah-display');
     counter.innerText = AlhamdulillahValue;
 })
-document.getElementById('alhamdulillah-decrement-btn').addEventListener('click',function(){
-    if(AlhamdulillahValue<=0){
+document.getElementById('alhamdulillah-decrement-btn').addEventListener('click', function () {
+    if (AlhamdulillahValue <= 0) {
         return alert('Only positive value Available!')
     }
     AlhamdulillahValue--;
@@ -38,16 +38,16 @@ document.getElementById('alhamdulillah-decrement-btn').addEventListener('click',
 })
 
 
-document.getElementById('allahu-akbar-increment-btn').addEventListener('click',function(){
-    if(AllahuAkberValue>=34){
+document.getElementById('allahu-akbar-increment-btn').addEventListener('click', function () {
+    if (AllahuAkberValue >= 34) {
         return alert('One Tasbih Complete, Press Reset Button And Start from 0')
     }
     AllahuAkberValue++;
     let counter = document.getElementById('allahu-akbar-display');
     counter.innerText = AllahuAkberValue;
 })
-document.getElementById('allahu-akbar-decrement-btn').addEventListener('click',function(){
-    if(AllahuAkberValue<=0){
+document.getElementById('allahu-akbar-decrement-btn').addEventListener('click', function () {
+    if (AllahuAkberValue <= 0) {
         return alert('Only positive value Available!')
     }
     AllahuAkberValue--;
@@ -55,7 +55,7 @@ document.getElementById('allahu-akbar-decrement-btn').addEventListener('click',f
     counter.innerText = AllahuAkberValue;
 })
 
-document.getElementById('reset-btn').addEventListener('click', function(){
+document.getElementById('reset-btn').addEventListener('click', function () {
     let SubhanAllahDisplay = document.getElementById('subhan-allah-display');
     let AlhamdulillahDisplay = document.getElementById('alhamdulillah-display');
     let AllahuAkbarDisplay = document.getElementById('allahu-akbar-display');
